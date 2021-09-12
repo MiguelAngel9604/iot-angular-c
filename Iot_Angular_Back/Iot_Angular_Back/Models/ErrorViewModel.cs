9858cfb5155936aa954536eb4e04bf228f370c0c
laulm <laurafablme@gmail.com>
2021-09-12 18:50:39 -0500

    feat: :sparkles: add c# folder for back
    Adding the c# folder for the back

---
+++ Iot_Angular_Back/Iot_Angular_Back/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Iot_Angular_Back.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
