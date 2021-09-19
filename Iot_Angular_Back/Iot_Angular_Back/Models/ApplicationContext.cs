bf5e0da40f78aa444086d62a880dbdfc63d837c7
Miguel Angel <angellm9604@gmail.com>
2021-09-19 14:04:27 -0500

    AppDBContext

---
+++ Iot_Angular_Back/Iot_Angular_Back/Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Iot_Angular_Back.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Users> users { get; set; }
+        public object Users { get; internal set; }
+    }
+
+}
